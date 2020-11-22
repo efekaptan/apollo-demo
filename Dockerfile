4e951e53850f3f076f436e12c3ef85be742856ef
Efe Kaptan <efekaptan@gmail.com>
2020-11-22 22:54:38 +0100

    Added Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM node:alpine
+WORKDIR '/app'
+COPY package*.json ./
+RUN npm install
+COPY . .
+RUN npm run build
+EXPOSE 80
+CMD ["npm", "run", "start"]
--- src/index.js
+++ src/index.js
@@ -11,6 +11,6 @@ const server = new ApolloServer({
 const app = express();
 server.applyMiddleware({ app });
 
-app.listen({ port: 4000 }, () =>
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+app.listen({ port: 80 }, () =>
+  console.log(`🚀 Server ready at http://localhost${server.graphqlPath}`)
 );
